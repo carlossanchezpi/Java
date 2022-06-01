88292e935beb3d7409acb7a1538cae94dc16431d
carlossanchezpi <carlos.sanchez@itgranada.com>
2022-06-01 13:47:00 +0200

    buscaminasfeoclase

---
+++ Buscaminas/Main.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
+public class Main{
+  public static void main(String[] args) {
+
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Jugador1 quieres cara o cruz?: ");
+    String respuesta=sc.nextLine().toUpperCase();
+    //if(respuesta.equals())
+
+
+
+
+  }
+}
